01bfde938b75b395523ddca22173ad24bab5c4f1
Nicolas Marshall <marshall.nicolas@gmail.com>
2024-11-17 14:09:14 +0000

    vscode: fix noir

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
               ORIGINAL_SETTINGS=$(if [[ $(file --mime "$SETTINGS_PATH") =~ "application/json" ]]; then cat "$SETTINGS_PATH"; else echo "{}"; fi)
               NEW_SETTINGS=`echo "$ORIGINAL_SETTINGS" \
                   | ${jq} ".\"noir.nargoPath\" |= \"$(which nargo)\"" \
-                  | ${jq} ".\"noir.enableLSP\" |= \"true\"" \
+                  | ${jq} ".\"noir.enableLSP\" |= true" \
               `;
               if [ "$(cat $SETTINGS_PATH)" != "$NEW_SETTINGS" ]; then
                   echo "$NEW_SETTINGS" >| "$SETTINGS_PATH"
