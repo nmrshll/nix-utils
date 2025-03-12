5badb0b80ec5232311fb15adc9d622ce8e9e3bfa
Nicolas M <marshall.nicolas@gmail.com>
2025-03-12 11:28:28 +0000

    scripts: git update branch (merge)

--- flake.nix
+++ flake.nix
@@ -29,9 +29,7 @@
       };
 
       scripts = with bin; myLib.mkScripts {
-        autorebase = ''#!/usr/bin/env bash
-          set -euxo pipefail
-
+        autorebase = ''set -euxo pipefail
           # This script will automatically rebase your branch onto main, by doing:
           #  - backup your current branch
           #  - pull latest changes on main
@@ -55,13 +53,13 @@
               >&2 echo "[ERROR]: Failed to fetch from remote '$REMOTE_NAME'"; 
               exit 1
           fi
-          MAIN_BRANCH=$(git -C "$REPO_PATH" symbolic-ref refs/remotes/''${REMOTE_NAME}/HEAD | sed "s@^refs/remotes/''${REMOTE_NAME}/@@")
+          MAIN_REMOTE_BRANCH=$(git -C "$REPO_PATH" symbolic-ref refs/remotes/''${REMOTE_NAME}/HEAD | sed "s@^refs/remotes/''${REMOTE_NAME}/@@")
           WORK_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-          TITLE_OF_FIRST_COMMIT_REBASED=$(git log --format=%B $(git merge-base $MAIN_BRANCH $WORK_BRANCH)..$WORK_BRANCH | tr -s '\n' | tail -n 1)
+          TITLE_OF_FIRST_COMMIT_REBASED=$(git log --format=%B $(git merge-base $MAIN_REMOTE_BRANCH $WORK_BRANCH)..$WORK_BRANCH | tr -s '\n' | tail -n 1)
 
           # check we're not on main
-          if [[ "''${WORK_BRANCH}" =~ "''${MAIN_BRANCH}" ]]; then echo "[ERROR] Branch can't be main branch" && exit 1; fi
+          if [[ "''${WORK_BRANCH}" =~ "''${MAIN_REMOTE_BRANCH}" ]]; then echo "[ERROR] Branch can't be main branch" && exit 1; fi
           # check everything committed
           if [ -n "$(git status --porcelain)" ]; then
               echo "[ERROR] There are uncommitted changes in working tree. Commit, then run this script again"
@@ -73,16 +71,51 @@
           git checkout -b "''${BACKUP_BRANCH}"
 
           # update main branch
-          git checkout "''${MAIN_BRANCH}"
+          git checkout "''${MAIN_REMOTE_BRANCH}"
           git pull
           git checkout "''${WORK_BRANCH}"
 
           # squash all changes of my branch
-          LAST_COMMON_COMMIT=$(git merge-base ''${WORK_BRANCH} ''${MAIN_BRANCH})
+          LAST_COMMON_COMMIT=$(git merge-base ''${WORK_BRANCH} ''${MAIN_REMOTE_BRANCH})
           git reset --soft ''${LAST_COMMON_COMMIT}
           git commit --all -m "$TITLE_OF_FIRST_COMMIT_REBASED"
 
-          git rebase "''${MAIN_BRANCH}"
+          git rebase "''${MAIN_REMOTE_BRANCH}"
+        '';
+        gupdate = ''set -x
+          REPO_PATH=$(git rev-parse --show-toplevel)
+          REMOTE_NAME=$(
+            NB_REMOTES=$(git -C "$REPO_PATH" remote | wc -l | tr -d '[:space:]')
+            if [ $NB_REMOTES -eq 1 ]; then
+              CURRENT_REMOTE=$(git -C "$REPO_PATH" remote | tr -d '[:space:]'); echo $CURRENT_REMOTE
+            else
+              >&2 echo "[ERROR]: no unique origin remote"; exit 1
+            fi;
+          );
+          if ! git fetch "$REMOTE_NAME" ; then
+              >&2 echo "[ERROR]: Failed to fetch from remote '$REMOTE_NAME'"; 
+              exit 1
+          fi
+          MAIN_REMOTE_BRANCH=$(git -C "$REPO_PATH" symbolic-ref refs/remotes/''${REMOTE_NAME}/HEAD | sed "s@^refs/remotes/''${REMOTE_NAME}/@@")
+          WORK_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+
+          echo "$REMOTE_NAME $MAIN_REMOTE_BRANCH $WORK_BRANCH"
+
+          # check we're not on main
+          if [[ "''${WORK_BRANCH}" =~ "''${MAIN_REMOTE_BRANCH}" ]]; then echo "[ERROR] Branch can't be main branch" && exit 1; fi
+          # check everything committed
+          if [ -n "$(git status --porcelain)" ]; then
+              echo "[ERROR] There are uncommitted changes in working tree. Commit, then run this script again"
+              exit 1
+          fi
+
+          # backup work branch
+          BACKUP_BRANCH="''${WORK_BRANCH}_backup_$(date +%y%m%d%H%M)"
+          git checkout -b "''${BACKUP_BRANCH}"
+
+          git fetch "$REMOTE_NAME"
+          git merge --no-edit "$REMOTE_NAME/$MAIN_REMOTE_BRANCH" || exit 1
         '';
 
         configure-editors = ''
