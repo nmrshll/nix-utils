657739a3a31aa2c7287bba04c6bb01aaf81b25dd
Nicolas M <marshall.nicolas@gmail.com>
2025-11-09 12:13:28 +0000

    add options devshell

---
+++ options/devshell.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, ... }: {
+  perSystem = { lib, pkgs, config, ... }: {
+    options = {
+      myDevShell = {
+        buildInputs = lib.mkOption {
+          type = lib.types.listOf lib.types.package;
+          default = [ ];
+          description = "Packages to add to the dev shell environment.";
+        };
+        shellHook = lib.mkOption {
+          type = lib.types.lines;
+          default = "";
+          description = "Lines to add to the shell hook script.";
+        };
+      };
+    };
+    config.devShells.default = lib.mkDefault (pkgs.mkShell {
+      buildInputs = lib.mkDefault config.myDevShell.buildInputs;
+      shellHook = lib.mkDefault config.myDevShell.shellHook;
+    });
+  };
+}
