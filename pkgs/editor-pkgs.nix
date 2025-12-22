51483d9c027bddfc85f66c2c00b4257b528147a5
Nicolas M <marshall.nicolas@gmail.com>
2025-12-22 18:15:47 +0100

    pkgs: gemini-cli (deprecated)

--- pkgs/editor-pkgs.nix
+++ pkgs/editor-pkgs.nix
@@ -32,6 +32,52 @@
       };
     };
   };
+
+  # NOW PART OF NIXPKGS
+  #  gemini-cli = { pkgs, lib, version ? "0.1.7", ... }:
+  #     let
+  #       versionDeps = {
+  #         early-access = { hash = "sha256-KNnfo5hntQjvc377A39+QBemeJjMVDRnNuGY/93n3zc="; npmDepsHash = "sha256-/IAEcbER5cr6/9BFZYuV2j1jgA75eeFxaLXdh1T3bMA="; };
+  #         "0.1.7" = { hash = "sha256-DAenod/w9BydYdYsOnuLj7kCQRcTnZ81tf4MhLUug6c="; npmDepsHash = "sha256-otogkSsKJ5j1BY00y4SRhL9pm7CK9nmzVisvGCDIMlU="; };
+  #         "0.1.5" = { hash = "sha256-JgiK+8CtMrH5i4ohe+ipyYKogQCmUv5HTZgoKRNdnak="; npmDepsHash = "sha256-yoUAOo8OwUWG0gyI5AdwfRFzSZvSCd3HYzzpJRvdbiM="; };
+  #       }.${version};
+  #     in
+  #     pkgs.buildNpmPackage {
+  #       name = "gemini-cli";
+  #       src = pkgs.fetchFromGitHub {
+  #         owner = "google-gemini";
+  #         repo = "gemini-cli";
+  #         tag = if lib.hasPrefix "0." version then "v${version}" else "${version}";
+  #         hash = versionDeps.hash;
+  #       };
+  #       npmDepsHash = versionDeps.npmDepsHash;
+
+  #       nativeBuildInputs = [ pkgs.typescript ];
+  #       fixupPhase = ''
+  #         runHook preFixup
+  #         find $out -type l -exec test ! -e {} \; -delete
+  #         runHook postFixup
+  #       '';
+  #       # nativeInstallCheckInputs = [
+  #       #   versionCheckHook
+  #       # ];
+  #       doInstallCheck = true;
+  #       versionCheckProgram = "${placeholder "out"}/bin/gemini";
+  #       versionCheckProgramArg = "--version";
+  #       meta = {
+  #         description = "Open-source AI agent that brings the power of Gemini directly into your terminal";
+  #         homepage = "https://github.com/google-gemini/gemini-cli";
+  #         changelog = "https://github.com/google-gemini/gemini-cli/releases/tag/v${version}";
+  #         license = lib.licenses.asl20;
+  #         maintainers = with lib.maintainers; [
+  #           ryota2357
+  #         ];
+  #         mainProgram = "gemini";
+  #       };
+  #     };
+
+
+
 }
 
 
